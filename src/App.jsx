d44837a84cd66799a31585dcded5319275d4c221
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-05-22 19:50:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button, Container } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Container maxW="620px">
+      <Button>hello</Button>
+    </Container>
+  );
+}
+
+export default App;

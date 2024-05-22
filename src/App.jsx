683b2faf3795080530771fa05860b3c9f57678c0
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-05-22 20:27:26 +0530

    logo toggle mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { Button, Container } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Container maxW="620px">
-      <Button>hello</Button>
+      <Header/>
+     <Routes>
+      <Route path="/:username" element={<UserPage/>}/>
+      <Route path="/:username/post/:pid" element={<PostPage/>}/>
+     </Routes>
     </Container>
   );
 }

b46edb1071a85eaf988f045ebafe7b453ea80628
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-05-29 00:23:58 +0530

    Update PostPage.jsx

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,11 +1,19 @@
-import { Avatar, Box, Button, Divider, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
 import Actions from "../components/Actions";
 import { useState } from "react";
 import Comment from "../components/Comment";
 
 const PostPage = () => {
-  const [liked,setLiked] = useState()
+  const [liked, setLiked] = useState();
   return (
     <>
       <Flex>
@@ -19,46 +27,60 @@ const PostPage = () => {
           </Flex>
         </Flex>
         <Flex gap={4} alignItems={"center"}>
-          <Text fontSize={"sm"} color={"gray.light"}>1d</Text>
-        <BsThreeDots />
+          <Text fontSize={"sm"} color={"gray.light"}>
+            1d
+          </Text>
+          <BsThreeDots />
         </Flex>
       </Flex>
       <Text my="3">Lets talk about threads</Text>
       <Box
-            borderRadius={6}
-            overflow={"hidden"}
-            border={"1px solid "}
-            borderColor={"gray.light"}
-          >
-            <Image src={"/post1.png"} w={"full"} />
-          </Box>
-          <Flex gap={3} my={3}>
-            <Actions liked={liked} setLiked={setLiked} />
-          </Flex>
-          <Flex gap={2} alignItems={"center"}>
-            <Text color={"gray.light"} fontSize={"sm"}>237 replies</Text>
-          <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-          <Text color={"gray.light"} fontSize={"sm"}>{237 + (liked? 1: 0)} likes</Text>
-          </Flex>
-          <Divider my={4}/>
-          <Flex justifyContent={"space-between"}>
-            <Flex gap={2} alignItems={"center"}>
-              <Text fontSize={"2xl"}> 👋 </Text>
-              <Text color={"gray.light"}>Get the app to like, replay, post and more</Text>
-            </Flex>
-            <Button>Get</Button>
-          </Flex>
-          <Divider my={4}/>
-          <Comment 
-          comment="looks really good 🎪"
-          createdAt="2d"
-          likes={200}
-          username="jondoe"
-          userAvatar='https://bit.ly/dan-abramov'
-          />
-         
+        borderRadius={6}
+        overflow={"hidden"}
+        border={"1px solid "}
+        borderColor={"gray.light"}
+      >
+        <Image src={"/post1.png"} w={"full"} />
+      </Box>
+      <Flex gap={3} my={3}>
+        <Actions liked={liked} setLiked={setLiked} />
+      </Flex>
+      <Flex gap={2} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          237 replies
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          {237 + (liked ? 1 : 0)} likes
+        </Text>
+      </Flex>
+      <Divider my={4} />
+      <Flex justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text fontSize={"2xl"}> 👋 </Text>
+          <Text color={"gray.light"}>
+            Get the app to like, replay, post and more
+          </Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+      <Divider my={4} />
+      <Comment
+        comment="looks really good 🎪"
+        createdAt="2d"
+        likes={200}
+        username="jondoe"
+        userAvatar="https://bit.ly/dan-abramov"
+      />
+      <Comment
+        comment="looks really good 🎪"
+        createdAt="2d"
+        likes={200}
+        username="jondoe"
+        userAvatar="https://bit.ly/dan-abramov"
+      />
     </>
   );
 };
 
- export default PostPage;
+export default PostPage;

246cdee712a7462b09e2570018b5ebd71f07a11a
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-03 19:33:06 +0530

    user update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,16 @@
 import express, { Router } from 'express';
-import {signupUser, loginUser, logoutUser, followUnfollowUser} from '../controllers/userController.js';
+import {signupUser, loginUser, logoutUser, followUnfollowUser, updateUser} from '../controllers/userController.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
+
+
 
 router.post("/signup",signupUser)
 router.post("/login",loginUser)
 router.post("/logout",logoutUser)
 router.post("/follow/:id",protectRoute,followUnfollowUser)
+router.post("/update/:id",protectRoute,updateUser)
 
 
 

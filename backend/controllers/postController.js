9beb1871f156207f41e4de6c8340e5643bc9735a
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-04 00:30:57 +0530

    Update postController.js

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,15 +1,34 @@
-const createPost = async(req,res)=>{
-    try {
-        const {postedBy, text, img} = req.body
+import User from "../models/userModel.js";
 
-        if(!postedBy || !text){
-            return res.status(400).json({message: "Postedby and Text fields are required"});
-        }
-        
-    } catch (err) {
-        res.status(500).json({message: err.message});
-        console.log("Error in createPost: ", err.message);
+const createPost = async (req, res) => {
+  try {
+    const { postedBy, text, img } = req.body;
+
+    if (!postedBy || !text) {
+      return res
+        .status(400)
+        .json({ message: "Postedby and Text fields are required" });
     }
+    const user = await User.findById(postedBy);
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-}
-export {createPost}
+    if (user._id.toString() !== req.user._id.toString())
+      return res.status(401).json({ message: "Unauthorized to create post" });
+
+    const maxLength = 500;
+    if (text.length > maxLength)
+      return res
+        .status(400)
+        .json({ message: `Text should be less than ${maxLength} characters` });
+
+    const newPost = new Post({ postedBy, text, img });
+
+    await newPost.save();
+    res.status(201).json({ message: "Post created successfully", newPost });
+  
+} catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in createPost: ", err.message);
+  }
+};
+export { createPost };

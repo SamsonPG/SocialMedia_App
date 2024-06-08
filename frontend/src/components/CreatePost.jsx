248cd40e81dbdcdc826725e7da1527b777b651f6
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-08 15:33:37 +0530

    create post

---
+++ frontend/src/components/CreatePost.jsx
@@ -0,0 +1,19 @@
+import { AddIcon } from "@chakra-ui/icons"
+import { Button, useColorModeValue } from "@chakra-ui/react"
+
+
+const CreatePost = () => {
+  return (
+    <>
+    <Button position={"fixed"}
+    bottom={10}
+    right={10}
+    leftIcon={<AddIcon/>}
+    bg={useColorModeValue("gray.300","gray.dark")}>
+        Post
+    </Button>
+    </>
+  )
+}
+
+export default CreatePost

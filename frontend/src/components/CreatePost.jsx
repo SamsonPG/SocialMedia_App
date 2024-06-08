fe8cb00d94fc7ef85404884a4d216361f3144305
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-08 15:49:41 +0530

    Update CreatePost.jsx

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,19 +1,69 @@
-import { AddIcon } from "@chakra-ui/icons"
-import { Button, useColorModeValue } from "@chakra-ui/react"
-
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  FormControl,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  Textarea,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const CreatePost = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [postText,setPostText] = useState("")
+
+  const handleTextChange = () => {};
+
   return (
     <>
-    <Button position={"fixed"}
-    bottom={10}
-    right={10}
-    leftIcon={<AddIcon/>}
-    bg={useColorModeValue("gray.300","gray.dark")}>
+      <Button
+        position={"fixed"}
+        bottom={10}
+        right={10}
+        leftIcon={<AddIcon />}
+        bg={useColorModeValue("gray.300", "gray.dark")}
+        onClick={onOpen}
+      >
         Post
-    </Button>
-    </>
-  )
-}
+      </Button>
 
-export default CreatePost
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <Textarea
+                placeholder="Post content goes here.."
+                onChange={handleTextChange}
+                value={postText}
+              />
+              <Text fontsize={"xm"}
+              fontWeight={"bold"}
+              textAlign={"right"}
+              m={"1"}
+              color={"gray.800"}>500/500</Text>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CreatePost;

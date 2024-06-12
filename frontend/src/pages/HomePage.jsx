83e90f31045f1bcb75964a0bb4d11667d23a5995
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-12 18:22:55 +0530

    home page feed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
-
+import {Button, Flex} from "@chakra-ui/react"
+import { useEffect } from "react"
+import{Link} from "react-router-dom"
+import useShowToast from "../hooks/useShowToast";
 
 const HomePage = () => {
+
+  const showToast = useShowToast();
+  useEffect(()=>{
+    const getFeedPost=()=>{
+      try {
+        
+      } catch (error) {
+        showToast("Error", error, "error");
+      }
+    }
+  },[])
   return (
-    <div>HomePage</div>
+   <Link to={"/"}>
+    <Flex w={"full"} justifyContent={"center"}>
+      <Button mx={"auto"}>Visit Profile Page</Button>
+    </Flex>
+   </Link>
   )
 }
 

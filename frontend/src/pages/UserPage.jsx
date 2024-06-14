9548b1f0726d5e2c9f1cf47070de4f1a1b3c623c
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-15 00:26:59 +0530

    Update UserPage.jsx

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -43,26 +43,7 @@ if(!user && loading){
   return (
     <>
       <UserHeader user={user} />
-      <UserPost
-        likes={1200}
-        replies={445681}
-        postImage="/post1.png"
-        postTitle="Lets talk about threads"
-      />
-      <UserPost
-        likes={451}
-        replies={86481}
-        postImage="/post2.png"
-        postTitle="oh great"
-      />
-      <UserPost
-        likes={150}
-        replies={481}
-        postImage="/post3.png"
-        postTitle="just awesome"
-      />
-      <UserPost likes={777} replies={41} postTitle="you are just legend" />
-    </>
+     </>
   );
 };
 

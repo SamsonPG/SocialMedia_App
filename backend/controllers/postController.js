a62641a42213a070f57c9445ac411687b17cde6f
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-16 00:23:08 +0530

    user posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -184,4 +184,23 @@ const replayToPost = async (req, res) => {
     console.log("Error in Replay to post", err.message);
   }
 };
-export {getFeedPosts, createPost, getPost, deletePost, likeUnlikePost, replayToPost };
+
+const getUserPosts = async(req,res)=>{
+  const{username} = req.params;
+  try {
+    const user = await User.findOne({username});
+    if(!user){
+      return res.status(404).json({error:"User not found"});
+    }
+    
+    const posts = await Post.find({postedBy: user._id}).sort({createdAt:-1})
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in get User post", error.message);
+  }
+}
+
+
+export {getFeedPosts, createPost, getPost, deletePost, likeUnlikePost, replayToPost ,getUserPosts};
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import UserHeader from "../components/UserHeader";
-import UserPost from "../components/UserPost";
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
 import { Spinner, Flex } from "@chakra-ui/react";
@@ -9,41 +8,68 @@ const UserPage = () => {
   const [user, setUser] = useState(null);
   const showToast = useShowToast();
   const { username } = useParams();
-  const [loading,setLoading]= useState(true)
+  const [loading, setLoading] = useState(true);
+  const [posts,setPosts] = useState([])
+  const [fetchingPosts, setFetchingPosts] = useState(true)
 
   useEffect(() => {
     const getUser = async () => {
       try {
         const res = await fetch(`/api/users/profile/${username}`);
         const data = await res.json();
-        console.log(data,username);
+        console.log(data, username);
         if (data.error) {
           showToast("Error", data.error, "error");
         }
         setUser(data);
       } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const getPosts = async () => {
+      setFetchingPosts(true)
+      try {
+        const res = await fetch("/api/posts/user/${username}");
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+        }
+        setPosts(data)
+      } catch (error) {
         showToast("Error", error, "error");
-      }finally{
-        setLoading(false)
+        setPosts([])
+      } finally {
+        setFetchingPosts(false);
       }
     };
 
     getUser();
+    getPosts();
   }, [username, showToast]);
 
-if(!user && loading){
-  return(
-    <Flex justifyContent="center"><Spinner size="xl"/></Flex>
-    
-  )
-}
+  if (!user && loading) {
+    return (
+      <Flex justifyContent="center">
+        <Spinner size="xl" />
+      </Flex>
+    );
+  }
 
   if (!user && !loading) return <h1>User not found</h1>;
 
   return (
     <>
       <UserHeader user={user} />
-     </>
+      {!fetchingPosts && posts.length === 0 && <h1>User has no posts.</h1>}
+    {fetchingPosts && (
+        <Flex justifyContent={"center"} my={12}>
+          <Spinner size={"xl"}/>
+          </Flex>
+    )}
+    </>
   );
 };
 

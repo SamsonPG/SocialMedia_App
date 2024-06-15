1a1b45003a37added8199939bdd05e4fb1a051a2
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-16 00:43:36 +0530

    Update UserHeader.jsx

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -29,7 +29,7 @@ const UserHeader = ({ user }) => {
   const currentUser = useRecoilValue(userAtom); //logged in user
   const [updating, setUpdating] = useState(false);
   const [following, setFollowing] = useState(
-    user.followers && user.followers.includes(currentUser._id)
+    user.followers && user.followers.includes(currentUser?._id)
   );
 
   const handleFollowUnfollow = async () => {
@@ -58,7 +58,7 @@ const UserHeader = ({ user }) => {
         user.followers.pop();
       } else {
         showToast("Success", `Followed ${user.name}`, "success");
-        user.followers.push(currentUser._id);
+        user.followers.push(currentUser?._id);
       }
       setFollowing(!following);
     } catch (error) {
@@ -126,12 +126,12 @@ const UserHeader = ({ user }) => {
       </Flex>
       <Text>{user.bio}</Text>
 
-      {currentUser._id === user._id && (
+      {currentUser?._id === user._id && (
         <Link as={RouterLink} to={"/update"}>
           <Button size={"sm"}>Update Profile</Button>
         </Link>
       )}
-      {currentUser._id !== user._id && (
+      {currentUser?._id !== user._id && (
         <Link>
           <Button
             size={"sm"}

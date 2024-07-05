385c4c2e5b97a6376863f1eb386d84fa770a0975
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-07-06 00:12:58 +0530

    get messages

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -35,8 +35,28 @@ try {
     res.status(201).json(newMessage);
  
 } catch (error) {
-    res.status(500).json({message : error.message});
+    res.status(500).json({error : error.message});
 }
 }
 
-export{sendMessage}
+async function getMessages(req,res){
+    const {otherUserId} = req.params;
+    const userId = req.user._id;
+    try {
+        const conversation = await Conversation.findOne({
+            participants:{$all:[userId, otherUserId]}
+        })
+        
+        if(!conversation) return res.status(404).json({error: "Conversation not found"});
+        
+        const messages = await Message.find({conversationId: conversation._id}).sort({createdAt: 1});
+        
+        res.status(200).json(messages);
+        
+    } catch (error) {
+    res.status(500).json({error : error.message});
+        
+    }
+}
+
+export{sendMessage,getMessages}
--- backend/models/conversationModel.js
+++ backend/models/conversationModel.js
@@ -6,7 +6,7 @@ const conversationSchema= new mongoose.Schema({
         ref: 'User'
     }],
     lastMessage:{
-        type: String,
+        text: { type: String },
         sender:{type: mongoose.Schema.Types.ObjectId, ref:'User'}
     },
  

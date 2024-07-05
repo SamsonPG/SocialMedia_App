5ae718ddbb517012c16226dd0d6330099fc83c27
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-07-05 20:24:16 +0530

    Update messageController.js

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,6 +1,11 @@
 async function sendMessage(req,res){
 try {
     const {recipientId, message} = req.body;
+    const senderId = req.user._id;
+
+    let conversation = await conversation.findOne({
+        
+    })
     
 } catch (error) {
     res.status(500).json({message : error.message});

08caf42570eb2b4b2d2e5b16a3f9c6afe02463e5
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-07-06 23:51:46 +0530

    set up conversations

--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -1,6 +1,27 @@
-import { Avatar, AvatarBadge, Flex, Image, Stack, Text, WrapItem, useColorModeValue } from "@chakra-ui/react";
+import {
+  Avatar,
+  AvatarBadge,
+  Flex,
+  Image,
+  Stack,
+  Text,
+  WrapItem,
+  useColorModeValue,
+  useColorMode,
+} from "@chakra-ui/react";
+import { useRecoilValue, useRecoilState } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { BsCheck2All } from "react-icons/bs";
+import { selectedConversationAtom } from "../atoms/messagesAtoms";
 
-const Conversation = ({conversation}) => {
+const Conversation = ({ conversation }) => {
+  const user = conversation.participants[0];
+  const currentUser = useRecoilValue(userAtom);
+  const lastMessage = conversation.lastMessage;
+  const [selectedConversation, setSelectedConversation] = useRecoilState(
+    selectedConversationAtom
+  );
+  const colorMode = useColorMode();
   return (
     <Flex
       gap={4}
@@ -11,22 +32,48 @@ const Conversation = ({conversation}) => {
         bg: useColorModeValue("gray.600", "gray.dark"),
         color: "white",
       }}
+      onClick={() =>
+        setSelectedConversation({
+          _id: conversation._id,
+          userId: user._id,
+          userProfilePic: user.profilePic,
+          username: user.username,
+        })
+      }
+      bg={
+        selectedConversation?._id === conversation._id
+          ? colorMode === "light"
+            ? "gray.400"
+            : "gray.dark"
+          : ""
+      }
       borderRadius={"md"}
     >
       <WrapItem>
-        <Avatar size={{
-           base:"xs",
-           sm:"sm",
-           md:"md" 
-        }} src="https://bit.ly/broken-link">
-        <AvatarBadge boxSize="1em" bg="green.500"/>
-  </Avatar>    </WrapItem>
+        <Avatar
+          size={{
+            base: "xs",
+            sm: "sm",
+            md: "md",
+          }}
+          src={user.profilePic}
+        >
+          <AvatarBadge boxSize="1em" bg="green.500" />
+        </Avatar>{" "}
+      </WrapItem>
       <Stack direction={"column"} fontSize={"sm"}>
         <Text fontWeight="700" display={"flex"} alignItems={"center"}>
-           {conversation.participants} <Image src='/verified.png' w={4} h={4} ml={1}/>
+          {user.username} <Image src="/verified.png" w={4} h={4} ml={1} />
         </Text>
         <Text fontSize={"xs"} display={"flex"} alignItems={"center"} gap={1}>
-            Hello Some Message...
+          {currentUser._id === lastMessage.sender ? (
+            <BsCheck2All size={16} />
+          ) : (
+            ""
+          )}
+          {lastMessage.text.length > 18
+            ? lastMessage.text.substring(0, 18) + "..."
+            : lastMessage.text}
         </Text>
       </Stack>
     </Flex>

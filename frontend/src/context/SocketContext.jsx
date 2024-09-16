bd44b4354367f7d372d94b167e93a5c645b4c19b
SamsonPG <samsonpg077@gmail.com>
2024-09-16 23:24:28 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,16 +1,23 @@
 import {createContext,useState,useEffect} from "react";
+import { useRecoilValue } from "recoil";
 import { io } from "socket.io-client";
+import userAtom from "../atoms/userAtom";
 
 const SocketContext= createContext();
 
 export const SocketContextProvider = ({children})=>{
     const[socket,setSocket]=useState(null);
-    
+    const user = useRecoilValue(userAtom)
     useEffect(()=>{
 const socket = io("http://localhost:3000",{
-    
+    query:{
+        userId:user?._id
+    }
+  
 })
-    },[])
+setSocket(socket)
+return()=> socket&& socket.close();
+    },[socket.user?._id])
 
     return (
         <SocketContext.Provider value={"hiii"}>
